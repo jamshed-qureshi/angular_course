512c064180bfa445ac8085e4b625753d863b315d
jamshed-qureshi <jamshed.alam_qureshi@yahoo.com>
2023-02-17 20:57:42 +0530

    8.TwoWayDataBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VDataBindingUsingInterpolationComponent } from './lessons/v-data-binding-using-interpolation/v-data-binding-using-interpolation.component'
 import { VIPropertyBindingComponent } from './lessons/vi-property-binding/vi-property-binding.component';
 import { VIIEventBindingComponent } from './lessons/vii-event-binding/vii-event-binding.component';
+import { VIIITwoWayDataBindingComponent } from './lessons/viii-two-way-data-binding/viii-two-way-data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VDataBindingUsingInterpolationComponent,
     VIPropertyBindingComponent,
-    VIIEventBindingComponent
+    VIIEventBindingComponent,
+    VIIITwoWayDataBindingComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/lessons/viii-two-way-data-binding/viii-two-way-data-binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viii-two-way-data-binding',
+  templateUrl: './viii-two-way-data-binding.component.html',
+  styleUrls: ['./viii-two-way-data-binding.component.css']
+})
+export class VIIITwoWayDataBindingComponent {
+ data = 'Angular';
+}
