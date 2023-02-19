3c54428da271801b10e3947e458aa76bbae99abf
jamshed-qureshi <jamshed.alam_qureshi@yahoo.com>
2023-02-19 18:26:29 +0530

    '10.ngIf_In_Angular'

---
+++ src/app/lessons/x-ng-if-in-angular/x-ng-if-in-angular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-x-ng-if-in-angular',
+  templateUrl: './x-ng-if-in-angular.component.html',
+  styleUrls: ['./x-ng-if-in-angular.component.css']
+})
+export class XNgIfInAngularComponent {
+  stockAvailable: boolean = true;
+}
